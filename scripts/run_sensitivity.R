72cd016742a7759db7b8cd340d400013360362db
dawnlwoodard <dawn.woodard@pnnl.gov>
2020-05-21 21:57:33 -0400

    fixed nomenclature issue with run_sensitivity code

--- scripts/run_sensitivity.R
+++ scripts/run_sensitivity.R
@@ -1,11 +1,13 @@
 library(hector)
+library(hectortools)
 library(stats)
 library(tidyverse)
+library(Runuran)
 
 source("scripts/analysis_tools.R")
 
-n_pts <- 50
-n_runs <- 100
+n_pts <- 100
+n_runs <- 1000
 outvar <- GLOBAL_TEMP()
 
 # Permafrost parameter priors. Currently assuming normal distribution. Should look into making more accurate.
@@ -18,8 +20,23 @@ priors <- tibble(fpf_static = urnorm(n_pts, 0.4, 0.2, lb=0, ub=1),
 
 # generate hector results from n_runs random samples from the priors and store in tibble
 hector_output <- run_all_param_sets(priors, n_runs, outvar)
+save(hector_output, file="hector_sensitivity_1000_runs.RData")
 
 # run Pecan-like sensitivity analysis over Hector results
-sensitivity_results <- run_sensitivity_analysis(out, names(out)[1], names(out)[-1])
+sensitivity_results <- run_sensitivity_analysis(hector_output, names(hector_output)[1], names(hector_output)[-1])
+save(sensitivity_results, file="sensitivity_results.RData")
 
 # make table of results
+library(xtable)
+sensitivity_results %>% 
+  mutate(param=recode_factor(param, fpf_static = "Non-labile Fraction", 
+                             pf_mu = "Frozen Permafrost Fraction Mean", 
+                             pf_sigma = "Frozen Permafrost Fraction Stdev", 
+                             permafrost_c = "Initial Permafrost Pool Size",
+                             rh_ch4_frac = "Methane Fraction from Thaw Decomp")) ->
+  sensitivity_results
+
+out_table <- xtable(sensitivity_results, digits=c(0, NA,2,2,4,2))
+names(out_table) <- c("Parameter", "Coefficient of Variation", "Elasticity", "Prediction Variance", "Partial Variance")
+print(out_table,floating=FALSE,latex.environments=NULL,booktabs=TRUE, include.rownames=FALSE, file="table.tex")
+
